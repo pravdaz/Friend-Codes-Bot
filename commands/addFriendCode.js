a26fedb6426503ef7d50c6e1196107e83ce09a30
pravdaz <68057316+pravdaz@users.noreply.github.com>
2021-12-03 16:38:24 +0100

    Update addFriendCode.js
    in french + message suppr

--- commands/addFriendCode.js
+++ commands/addFriendCode.js
@@ -7,7 +7,7 @@ const addFriendCode = async (friendcode, msg, db) => {
     regex.exec(friendcode) === null ||
     friendcode.length < 12
   ) {
-    return msg.reply(`Invalid friend code ${friendcode}`);
+    return msg.reply(`Code ami invalide ${friendcode}`);
   }
 
   // Try inserting username/friendcode into db
@@ -16,16 +16,22 @@ const addFriendCode = async (friendcode, msg, db) => {
       username: `${msg.author.username}`,
       pogocode: friendcode,
     });
+    
+    // Delete a message
+    msg.delete()
+    .then(msg => console.log(`Deleted message from ${msg.author.username}`))
+    .catch(console.error);
+    // return 
     return msg.reply(
-      `Friend code ${insertFriendCode.pogocode} added for ${insertFriendCode.username}`
+      `Code ami ${insertFriendCode.pogocode} ajouté` //pour ${insertFriendCode.username}
     );
   } catch (e) {
     // Return error if friendcode already exists
     if (e.name === "SequelizeUniqueConstraintError") {
-      return msg.reply("That friendcode already exists.");
+      return msg.reply("Ce code ami existe déjà.");
     }
     console.log("error adding friend code:", e);
-    return msg.reply("Something went wrong with adding a friend code.");
+    return msg.reply("Quelque chose s'est mal passé en ajoutant votre code ami...");
   }
 };
 
